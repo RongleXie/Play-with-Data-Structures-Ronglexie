a7d9a0bce7e6c063a63357a7203b497ca52f6e86
ronglexie <252254002@qq.com>
2018-08-12 17:49:01 +0800

    :sparkles:新增链表

---
+++ 04-Linked-List/src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * @author ronglexie
+ * @version 2018/8/12
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> linkedList = new LinkedList<>();
+		for (int i = 0; i < 10; i++) {
+			linkedList.addFirst(i);
+			System.out.println(linkedList.toString());
+		}
+		linkedList.add(5,11111);
+		System.out.println(linkedList.toString());
+
+		linkedList.remove(5);
+		System.out.println(linkedList.toString());
+		linkedList.removeFirst();
+		System.out.println(linkedList.toString());
+		linkedList.removeLast();
+		System.out.println(linkedList.toString());
+	}
+}
